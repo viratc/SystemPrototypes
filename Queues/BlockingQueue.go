07026ea371b83c187aeebb751818981f0d05b8df
Virat Chourasia <virat.viratc@gmail.com>
2024-06-25 15:22:05 +0530

    Added Enque and Deque for Go Queue

--- Queues/BlockingQueue.go
+++ Queues/BlockingQueue.go
@@ -9,9 +9,47 @@ type BlockingQueue struct {
 	capacity int
 }
 
-func new_blocking_queue(capacity int) *BlockingQueue {
+// Constructor
+func NewBlockingQueue(capacity int) *BlockingQueue {
 	q := new(BlockingQueue)
 	q.c = sync.Cond{L: &q.m}
 	q.capacity = capacity
 	return q
+}
+
+func (q *BlockingQueue) isFull() bool {
+	return len(q.data) == q.capacity
+}
+
+func (q *BlockingQueue) Enqueue(item interface{}) {
+	q.c.L.Lock()
+	defer q.c.L.Unlock()
+
+	for q.isFull() {
+		q.c.Wait()
+	}
+
+	q.data = append(q.data, item)
+	q.c.Signal()
+}
+
+func (q *BlockingQueue) isEmpty() bool {
+	return len(q.data) == 0
+}
+
+func (q *BlockingQueue) Dequeue() interface{} {
+
+	q.c.L.Lock()
+	defer q.c.L.Unlock()
+
+	for q.isEmpty() {
+		q.c.Wait()
+	}
+
+	result := q.data[0]
+
+	// Remove the first element from FIFO queue
+	q.data = q.data[1:len(q.data)]
+
+	return result
 }
